e3f00a848dc79896e2070728e9eeb516f0d0e9cb
Jakub Petrík <petrik@inloop.eu>
2016-10-15 01:09:09 +0200

    Initial commit

---
+++ MIMEResolver/MIMEResolver.h
@@ -0,0 +1,15 @@
+//
+//  MIMEResolver.h
+//  MIMEResolver
+//
+//  Created by Jakub Petrík on 10/14/16.
+//  Copyright © 2016 Inloop, s.r.o. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MIMEResolver.
+FOUNDATION_EXPORT double MIMEResolverVersionNumber;
+
+//! Project version string for MIMEResolver.
+FOUNDATION_EXPORT const unsigned char MIMEResolverVersionString[];
---
+++ MIMEResolverTests/MIMEResolverTests.swift
@@ -0,0 +1,14 @@
+//
+//  MIMEResolverTests.swift
+//  MIMEResolverTests
+//
+//  Created by Jakub Petrík on 10/14/16.
+//  Copyright © 2016 Inloop, s.r.o. All rights reserved.
+//
+
+import XCTest
+@testable import MIMEResolver
+
+class MIMEResolverTests: XCTestCase {
+    
+}
