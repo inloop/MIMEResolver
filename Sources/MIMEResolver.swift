a75856190f482deb82da0ec253989a27a80d96d0
Jakub Petrík <petrik@inloop.eu>
2016-10-15 01:21:40 +0200

    Add MIMEResolver

---
+++ Sources/MIMEResolver.swift
@@ -0,0 +1,11 @@
+//
+//  MIMEResolver.swift
+//  MIMEResolver
+//
+//  Created by Jakub Petrík on 10/15/16.
+//  Copyright © 2016 Inloop, s.r.o. All rights reserved.
+//
+
+public struct MIMEResolver {
+
+}
