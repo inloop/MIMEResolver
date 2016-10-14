4edcabd86795380b1493879b2e6541ed6a0a9e3e
Jakub Petrík <petrik@inloop.eu>
2016-10-15 01:19:08 +0200

    Add MIME interface

---
+++ Sources/MIME.swift
@@ -0,0 +1,12 @@
+//
+//  MIME.swift
+//  MIMEResolver
+//
+//  Created by Jakub Petrík on 10/15/16.
+//  Copyright © 2016 Inloop, s.r.o. All rights reserved.
+//
+
+protocol MIME {
+    var signature: [UInt8] { get }
+    var contentType: String { get }
+}
